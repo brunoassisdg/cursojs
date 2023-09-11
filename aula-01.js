f59c020c15d6a0d041653cdd1688116ee6669d18
bruno assis <101024960+brunoassisdg@users.noreply.github.com>
2023-09-11 17:28:01 -0300

    Aula JS

---
+++ aula-01.js
@@ -0,0 +1,8 @@
+const x = 1;
+const y = 3;
+
+console.log('resultado:', x + y)
+
+const soma = x + y;
+
+console.log('resultado>', soma)
---
+++ aula-03.js
@@ -0,0 +1,19 @@
+const idade = 18;
+const nome = 'Bruno';
+//let podeEntrar = false;
+
+if(idade >= 18 && nome === 'Brun0'){
+    //podeEntrar = true;
+    console.log('ENTROU!!!')
+} else {
+    console.log('NÃO PODE ENTRAR!!!')
+}
+
+if(idade >= 18 || nome === 'Bruno'){
+    //podeEntrar = true;
+    console.log('ENTROU 2 !!!')
+} else {
+    console.log('NÃO PODE ENTRAR!!!')
+}
+
+//console.log('usuario pode entrar?', podeEntrar)
